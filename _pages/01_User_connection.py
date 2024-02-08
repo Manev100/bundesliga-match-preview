a27e0e0293d287fa253c9fb43156a153f60bcde8
Marc Neveling <marc.neveling@hhu.de>
2024-02-08 18:39:44 +0100

    Initial setup

---
+++ _pages/01_User_connection.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title='User Connection setup',
+    page_icon='🧰'
+)
+
+st.title("🧰 Connection setup")
---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+st.set_page_config(
+    page_title='Bundesliga Match Preview',
+    page_icon='⚽',
+   #  layout="wide"
+)
+
+
+st.title('Bundesliga Match Preview')
+
+
+st.write("Choose upcoming match for preview")
